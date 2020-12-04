6f3bc2ebd31304ce315cfde693d7c7affc6380e2
jefferson <jeffer.leo93@gmail.com>
2020-12-03 18:45:35 -0600

    Mesas en vivo

--- static/src/js/pos_guardar.js
+++ static/src/js/pos_guardar.js
@@ -955,14 +955,20 @@ floors.TableWidget.include({
             })
             .then(function (ordenes){
                 var cantidad_clientes = 0;
+                self.cantidad_ordenes = 0;
+                self.cantidad_clientes = 0;
                 if (ordenes.length > 0 ){
                     self.cantidad_ordenes = ordenes.length;
                     for (var i = 0; i < ordenes.length; i++){
                         cantidad_clientes += ordenes[i].customer_count
                     }
                     self.cantidad_clientes = cantidad_clientes;
-                    self.renderElement();
+                    // self.renderElement();
                 }
+                self.renderElement();
+                // else{
+                //     self.renderElement();
+                // }
             });
     },
     renderElement: function(){
@@ -970,7 +976,6 @@ floors.TableWidget.include({
         this._super();
         var intervalor = setInterval(function() {
             self.obtener_cantidad()
-            self.renderElement()
             clearInterval(intervalor);
         }, 33000);
 
